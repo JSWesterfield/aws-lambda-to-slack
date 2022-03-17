ed3d5635191a3840ef7555c1b7cbfa07b844f8f2
Jake Westerfield <Jacob_Westerfield@discovery.com>
2022-03-17 17:46:03 -0400

    add the logic in our lambda function, checks if there is a file attached to the message and reply if it is the case

--- lambda_function.py
+++ lambda_function.py
@@ -11,3 +11,33 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': challenge
     }
+
+def is_bot(event): 
+    return 'bot_profile' in event['event']
+
+def has_file(event):
+    return 'files' in event['event']
+
+def message_has_resume(event): 
+    return not is_bot(event) and has_file(event)
+
+def send_text_response(event, response_text):
+    SLACK_URL = "https://slack.com/api/chat.postEphmeral" # use chat.postMessage if we want visible for everyBody
+    channel_id = event["event"]["channel"]
+    user = event["event"]["user"]
+    bot_token = os.environ["BOT_TOKEN"]
+    data = urllib.parse.urlencode({
+        "token": bot_token,
+        "channel": channel_id,
+        "text": response_text,
+        "user": user,
+        "link_names": True
+    })
+    data = data.encode("ascii")
+    request = urllib.request.Request(
+        SLACK_URL, 
+        data=data,
+        method="POST"
+    )
+    res = urllib.request.urlopen(request).read()
+    print('res:', res)
