985865b2dac4614cf78fd5897a87bf9e524dfa36
Jake Westerfield <Jacob_Westerfield@discovery.com>
2022-03-17 17:28:22 -0400

    adds in comments

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,8 @@ import json
 import os
 import urllib
 
+# initial challenge, def lambda_handler(event, context) is the entrypoint of every lambda function. 
+# The event parameter gives you info about the event that triggered the function (the slack API post request in our case)
 def lambda_handler(event, context):
     slack_event = json.loads(event["body"])
     challenge = slack_event["challenge"]
